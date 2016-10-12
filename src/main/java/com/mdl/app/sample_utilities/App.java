0a100a705b71336f405d0fd7d46cdcfad9dcf1c3
dongliang.ma <dongliang.ma@baozun.cn>
2016-10-12 16:12:25 +0800

    commit again3

---
+++ src/main/java/com/mdl/app/sample_utilities/App.java
@@ -0,0 +1,14 @@
+package com.mdl.app.sample_utilities;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "commit again3!" );
+    }
+}
